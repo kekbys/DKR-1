a7655551a4bc8a030cb5dc8958eeb29b50b26ff8
Egor <117011554+kekbys@users.noreply.github.com>
2022-11-11 13:02:49 +0300

    Add files via upload

---
+++ лаба.pas
@@ -0,0 +1,12 @@
+﻿var x,k,e:real;
+begin
+  x:= -11;
+  k:= 0;
+  while x<k do
+  begin
+    if (x < -9) then writeln('X=', x:2:2, 'f(x)= ',(power(e, x)- log(x)/log(10)));
+    if (-9 <= x) and (x < -2) then writeln('X=',x:2:2, 'f(x)= ',(log(x)/log(10) * sin(x) / cos(x) - x));
+    if (-2 <= x) then writeln('X=',x:2:2, 'f(x)= ',(power(x,0.1*x)*1+x/1-x + cos(2 * x)/power(x, 0.1*x)));
+    x:= x + 0.3;
+  end;
+end.
